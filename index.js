58cc51d72f6d126e46a1fe0151ba354620f9e931
Adam Kabak <adamkabak@yahoo.com>
2022-09-19 13:38:32 -0400

    commit tree 00c931c0ab50270b9318dff03880914239d6a1b2

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+    if (location < 42) return 42 - location;
+    if (location > 42) return location - 42;
+    return 0;
+}
+
+function distanceFromHqInFeet(location) {
+    const blocks = distanceFromHqInBlocks(location);
+    return blocks * 264;
+}
+
+function distanceTravelledInFeet(start, end) {
+    if (start < end) return (end - start) * 264;
+    if (start > end) return (start - end) * 264;
+    return 0;
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    const chargedDistance = distance - 400;
+    if (distance > 2500) return 'cannot travel that far';
+    if (distance <= 400) return 0;
+    if (distance > 400 && distance <= 2000) return chargedDistance * .02;
+    return 25;
+}
